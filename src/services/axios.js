25d9a299dda13597c4f618db01b48b7b1987b543
kauanps <kauanps2271@gmail.com>
2025-02-20 15:47:57 -0300

    antes do deploy

--- src/services/axios.js
+++ src/services/axios.js
@@ -3,8 +3,8 @@ import axios from "axios";
 
 // Configuração do axios para consumir a API do localhost
 const api = axios.create({
-  baseURL: "http://localhost:3000/", // Substitua pela sua URL da API local
-  timeout: 1000, // Timeout de 10 segundos
+  baseURL: "http://52.55.211.70:3000", // Substitua pela sua URL da API local
+  timeout: 10000, // Timeout de 10 segundos
 });
 
 export default api;
